d73028c97b46dca59bfa8976759f99f297fbdb74
Shivam Chaudhary <shivamchaudhary75@gmail.com>
2023-08-05 20:40:43 +0530

    changes

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -1,3 +1,11 @@
+// import React from "react";
+
+// const UserPage = () => {
+//   return <div>UserPage</div>;
+// };
+
+// export default UserPage;
+
 import React from "react";
 
 const UserPage = () => {
